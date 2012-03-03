7f25162fe6c4e77491ee21445f142846bdc54cb4
Richard Flosi <richard.flosi@gmail.com>
2012-03-03 02:24:57 -0800

    added about stub

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/about', routes.about);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'SmashBomb' })
+  res.render('index', {title: 'SmashBomb'})
+};
+exports.about = function(req, res){
+  res.render('about', {title: 'About'})
 };
